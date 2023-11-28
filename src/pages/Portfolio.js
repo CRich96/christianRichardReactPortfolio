a32ba7126d4413407875cb0a432c1108471ced6c
CRich96 <chris575502@icloud.com>
2023-11-28 14:23:14 -0600

    add two more porjects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import "./pages.css";
+
 
 
 export default function Portfolio() {
     return (
         <div class="row row-cols-1 row-cols-sm-3 g-4">
             <div className="col">
-                <div className="card w-60 h-40">
+                <div className="card w-60 h-40 m-5">
                     <img
                         src="https://images.unsplash.com/photo-1472114864173-39596323454f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                         class="card-img-top"
@@ -37,7 +37,7 @@ export default function Portfolio() {
                 </div>
             </div>
             <div className="col">
-                <div className="card w-60 h-40">
+                <div className="card w-60 h-40 m-5">
                     <img
                         src="https://images.unsplash.com/photo-1509475826633-fed577a2c71b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
                         class="card-img-top"
@@ -68,9 +68,65 @@ export default function Portfolio() {
                 </div>
             </div>
             <div className="col">
-                <div className="card w-60 h-40">
+        <div className="card w-60 h-40 m-5">
+          <img
+            src="https://images.unsplash.com/photo-1509475826633-fed577a2c71b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+            class="card-img-top"
+            alt="Palm Springs Road"
+          />
+          <div className="card-body">
+            <h5 className="card-title">SVG Logo Maker</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div>
+              <a
+                href="https://github.com/CRich96/Christian-Richard-SVG-Project"
+                className="btn btn-primary"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card w-60 h-40 m-5">
+          <img
+            src="https://images.unsplash.com/photo-1509475826633-fed577a2c71b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+            class="card-img-top"
+            alt="Palm Springs Road"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Note Taker App</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div>
+              <a
+                href="https://deploy-note-taker-cad320294d76.herokuapp.com/"
+                className="btn btn-primary"
+              >
+                App
+              </a>
+              <a
+                href="https://github.com/CRich96/ExpressJS-NoteTaker"
+                className="btn btn-primary"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+            <div className="col">
+                <div className="card w-60 h-40 m-5">
                     <img
-                        src="https://images.unsplash.com/photo-1506784983877-45594efa4cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80"
+                        src="https://images.unsplash.com/photo-1506784983877-45594efa4cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                         class="card-img-top"
                         alt="Los Angeles Skyscrapers"
                     />
@@ -98,7 +154,7 @@ export default function Portfolio() {
                 </div>
             </div>
             <div className="col">
-                <div className="card w-60 h-40">
+                <div className="card w-60 h-40 m-5">
                     <img
                         src="https://images.unsplash.com/photo-1634224143538-ce0221abf732?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                         class="card-img-top"
@@ -129,17 +185,5 @@ export default function Portfolio() {
                 </div>
             </div>
         </div>
-        //   <div className="aboutMe">
-        //     <img src={ProfilePic} className="profile-picture" alt="profile" />
-        //     <p className="aboutMe">
-        //       Hi, I'm Christian, full-stack web developer.
-        //       <br />
-        //       I am in this industry to solve real-world problems with my applications.
-        //       <br />
-        //       I am currently based in Dallas, TX.
-        //       <br />
-        //       You are welcome to look around.
-        //     </p>
-        //   </div>
     );
 }
