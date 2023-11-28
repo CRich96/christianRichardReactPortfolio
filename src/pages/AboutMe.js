274d0d3f154ec09214d2df415694190f44815ac6
CRich96 <chris575502@icloud.com>
2023-11-28 14:25:25 -0600

    update

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,10 +4,10 @@ import ProfilePic from "../assets/profilePic.png";
 
 export default function AboutMe() {
   return (
-    <div className="aboutMe">
+    <div className="aboutMe aboutMeContainer">
       <img src={ProfilePic}
       className="profile-picture" alt="profile" />
-      <p className="aboutMe">
+      <p className="aboutMe text">
         Hi, I'm Christian, full-stack web developer.
         <br />
         I am in this industry to solve real-world problems with my applications.
